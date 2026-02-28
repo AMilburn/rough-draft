6326cfedf0283f0063d41c63cf25e5ab7824a2b2
AMilburn <AMilburn@users.noreply.github.com>
2026-02-28 22:44:06 +0100

    Updates

---
+++ _next/static/dXDJQvglfOnk_EbgM_UEM/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/rough-draft//_next/:path+",
+        "destination": "/rough-draft/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
